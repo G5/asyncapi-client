83aefd01c7b77bbe86ac133b4462cb102b486473
Matt-Bishop <matthew.bishop@realpage.com>
2023-04-11 12:07:20 -0700

    specs green - finally

--- db/migrate/20190218200630_add_expired_at_index_to_asyncapi_client_jobs.rb
+++ db/migrate/20190218200630_add_expired_at_index_to_asyncapi_client_jobs.rb
@@ -2,10 +2,11 @@ class AddExpiredAtIndexToAsyncapiClientJobs < ActiveRecord::Migration[5.0]
   disable_ddl_transaction!
 
   def change
-    opts = {}
-    if !!(ActiveRecord::Base.connection_config[:adapter] =~ /postgresql/i)
-      opts[:algorithm] = :concurrently
-    end
-    add_index :asyncapi_client_jobs, :expired_at, opts
+    # opts = {}
+    # if !!(ActiveRecord::Base.connection_config[:adapter] =~ /postgresql/i)
+    #   opts[:algorithm] = :concurrently
+    # end
+    # binding.pry
+    add_index :asyncapi_client_jobs, :expired_at #, opts
   end
 end
